4a75f03246fa6391d0dccca80a8aea7070ce611f
Iylia Hani Rostam <Iylia@ISKL-ERP.iskl.edu.my>
2022-10-26 08:35:49 +0800

    Variable Homework 19 Oct 22

---
+++ solution_variable.py
@@ -0,0 +1,54 @@
+
+beeps = 2
+boops = 6
+total = beeps + boops
+print(total) # prints 8 H
+
+beeps = 0
+boops = 5
+total = beeps + boops
+print(total) # prints 5 E
+
+beeps = 9
+boops = 3
+total = beeps + boops
+print(total) # prints 12 L
+
+beeps = 4
+boops = 8
+total = beeps + boops
+print(total) # prints 12 L
+
+beeps = 10
+boops = 5
+total = beeps + boops
+print(total) # prints 15 O
+
+beeps = " "
+print(beeps) # prints
+
+beeps = 11
+boops = 12
+total = beeps + boops
+print(total) # prints 23 W
+
+beeps = 5
+boops = 5
+total = beeps + boops
+print(total) # prints  1O J
+
+beeps = 1
+boops = 17
+total = beeps + boops
+print(total) # prints 18 R
+
+beeps = 5
+boops = 7
+total = beeps + boops
+print(total) # prints  12 L
+
+
+beeps = 4
+boops = 0
+total = beeps + boops
+print(total) # prints 4 D
