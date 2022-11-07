ca4500af61a3249f2426cdd0de7fa4811e24f3da
Iylia Hani Rostam <Iylia@ISKL-ERP.iskl.edu.my>
2022-11-07 20:25:18 +0800

    Solution 1 2 Nov 22

---
+++ Solution1_2Nov.py
@@ -0,0 +1,11 @@
+time = 23 #1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24
+if time < 9:
+    print ("Morning is wonderful. Its only drawback is that it comes at such an inconvenient time of day.")
+elif time <= 16:
+           print ("Working hard or hardly working?")
+elif time < 20:
+           print ("How did it get so late so soon?")
+elif time <= 22:
+           print ( "A day without sunshine is like, you know, night.")
+else:
+           print ( "Burning the midnight oil!")
