1e07ad241eb94cf4e9c43c3611e5e703d9692f33
Iylia Hani Rostam <Iylia@ISKL-ERP.iskl.edu.my>
2022-11-16 07:48:17 +0800

    Homework solution 2 9Nov22

---
+++ Solution2_9Nov22_.py
@@ -0,0 +1,8 @@
+temperature = 90
+
+while temperature > 75 :
+    print("The temperature is",temperature, "crack the AC!")
+    temperature = temperature - 3
+
+    if temperature == 75:
+        print(temperature," Ahh, that's better.")
